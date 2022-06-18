5c4b766dffdda6b56df1c88b39b037e25cdc9bad
johnk2280 <johnk2280@gmail.com>
2022-06-18 16:58:03 +0500

    [init] start project

---
+++ numbers_dashboard/mainapp/models.py
@@ -0,0 +1,26 @@
+import decimal
+
+from django.db import models
+
+
+class Order(models.Model):
+    order_number = models.BigIntegerField(
+        verbose_name='Номер заказа',
+    )
+    usd_price = models.DecimalField(
+        verbose_name='Стоимость, $',
+        max_digits=15,
+        decimal_places=2,
+    )
+    rur_price = models.DecimalField(
+        verbose_name='Стоимость, руб',
+        max_digits=15,
+        decimal_places=2,
+    )
+    delivery_date = models.DateField()
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
